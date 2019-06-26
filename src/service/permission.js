580ee32d05e375328a610e64cb007d8896a9fc46
wangbing <wangbin01@wondersgroup.com>
2019-06-26 11:46:38 +0800

    修改接口请求形式

--- src/service/permission.js
+++ src/service/permission.js
@@ -1,13 +1,12 @@
 import { Ajax } from '@/common'
-import qs from 'qs';
 
 export default {
   // 获取符合用户的菜单列表
   getPermission(query) {
-    return Ajax.get(`/menu/findNavTree`, query)
+    return Ajax.post(`/menu/findNavTree`, query)
   },
   // 获取符合用户的按钮列表
   getButtonsList(query) {
-    return Ajax.get(`/user2/findPermissions`, query)
+    return Ajax.post(`/user2/findPermissions`, query)
   },
 }
